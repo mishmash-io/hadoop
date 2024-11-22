bc6097290f3200b46e063578153f4c20c2e90d70
zzuljin <zethix@gmail.com>
2024-11-22 16:29:38 +0200

    Update to log4j 2

--- hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/security/ssl/TestReloadingX509KeyManager.java
+++ hadoop-common-project/hadoop-common/src/test/java/org/apache/hadoop/security/ssl/TestReloadingX509KeyManager.java
@@ -45,14 +45,14 @@ public class TestReloadingX509KeyManager {
     private static final String BASEDIR = GenericTestUtils.getTempPath(
             TestReloadingX509TrustManager.class.getSimpleName());
 
-    private final GenericTestUtils.LogCapturer reloaderLog = GenericTestUtils.LogCapturer.captureLogs(
-            FileMonitoringTimerTask.LOG);
+    private static GenericTestUtils.LogCapturer reloaderLog;
 
     @BeforeAll
     public static void setUp() throws Exception {
         File base = new File(BASEDIR);
         FileUtil.fullyDelete(base);
         base.mkdirs();
+        reloaderLog = GenericTestUtils.LogCapturer.captureLogs(FileMonitoringTimerTask.LOG);
     }
 
     @Test
