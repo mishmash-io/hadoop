bec79c84d34dc1fc746de348be6c1ba2dc17facc
zzuljin <zethix@gmail.com>
2024-11-30 13:04:03 +0200

    Upgrade to commons http 5

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/TransferFsImage.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/TransferFsImage.java
@@ -56,7 +56,7 @@ import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.hadoop.security.authentication.client.AuthenticationException;
 import org.apache.hadoop.util.Lists;
 import org.apache.hadoop.util.Time;
-import org.apache.http.client.utils.URIBuilder;
+import org.apache.hc.core5.net.URIBuilder;
 
 import org.apache.hadoop.classification.VisibleForTesting;
 import org.eclipse.jetty.io.EofException;
